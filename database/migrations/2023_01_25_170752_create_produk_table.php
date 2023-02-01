3e77230820341c8c48f543fbe75a7e6a17600347
natashafortunata <natasha.fortunata@gmail.com>
2023-02-01 19:54:00 +0700

    pertama

---
+++ database/migrations/2023_01_25_170752_create_produk_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->id('kode_produk');
+            $table->string('nama');
+            $table->string('jenis');
+            $table->string('warna');
+            $table->string('bahan');
+            $table->longText('deskripsi');
+            $table->integer('harga');
+            $table->integer('stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produk');
+    }
+}
